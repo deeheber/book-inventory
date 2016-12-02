8a725a5c87a98792912de5b0b3d597860e5fafd3
Danielle H <daheber@gmail.com>
2016-12-01 20:36:02 -0800

    incoming data filtered in delete.php

--- delete.php
+++ delete.php
@@ -2,8 +2,11 @@
     include("connect.php");
     
     try {
-        $id = $_GET['id'];
-        $result = $db->query("DELETE FROM books WHERE id=$id");
+        $id = filter_input(INPUT_GET, 'id', FILTER_SANITIZE_NUMBER_INT);
+        $sql = "DELETE FROM books WHERE id=?";
+        $result = $db->prepare($sql);
+        $result->bindValue(1, $id, PDO::PARAM_INT);
+        $result->execute();
     } catch (Exception $e) {
         echo "Unable to delete book, error in db. </br>";
         echo "Error: ".$e->getMessage()."</br>";
